53d76af5e06306d6403e8a89caedd6788a5b0e19
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-22 14:40:37 -0400

    adds up and down functionality

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Your code here
 let dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+// document.addEventListener("keydown", function(e) {
+//     console.log(e.key);
+//   });
 
 function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
@@ -12,11 +17,30 @@ function moveDodgerLeft() {
 
  function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '');
+  console.log(leftNumbers)
   var left = parseInt(leftNumbers, 10)
   if (left > 0) {
     dodger.style.left = `${left + 1}px`
   }
 }
+
+function moveDodgerDown() {
+    var bottomNumbers = dodger.style.bottom.replace('px', '');
+    console.log(bottomNumbers)
+    var bottom = parseInt(bottomNumbers, 10)
+    if (bottom > 0) {
+      dodger.style.bottom = `${bottom - 1}px`
+    }
+  }
+
+  function moveDodgerUp() {
+    var upNumbers = dodger.style.bottom.replace('px', '');
+    var up = parseInt(upNumbers, 10)
+    console.log(up)
+    if (up >= 0) {
+      dodger.style.bottom = `${up + 1}px`
+    }
+  }
 
   document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
@@ -24,5 +48,11 @@ function moveDodgerLeft() {
   }
   if (e.key === "ArrowRight") {
     moveDodgerRight();
+  }
+  if (e.key === "ArrowDown") {
+    moveDodgerDown();
+  }
+  if (e.key === "ArrowUp") {
+    moveDodgerUp();
   }
 });
