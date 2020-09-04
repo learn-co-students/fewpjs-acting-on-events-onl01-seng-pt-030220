a1c72bd3a59a93c969ac5f8f212eacc9a28a3bde
aredmond85 <aredmond85@gmail.com>
2020-09-03 21:00:00 -0500

    Done.

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft();
     }
-    
 });
 
 document.addEventListener("keydown", function(e) {
