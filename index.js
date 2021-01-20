31339e853c7c87a22020cec4149d4b579ed185c0
AHMED <eng.computer1@yahoo.com>
2021-01-20 14:22:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor="#FF69B4"
+console.log(dodger.style.left); // "180px"
+console.log(dodger.style.bottom); // "0px"
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+
 function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "")
     let left = parseInt(leftNumbers, 10);
@@ -18,3 +27,16 @@ function moveDodgerRight() {
     }
 
 }
+
+document.addEventListener("keydown",function(e){
+   if(e.key == 'ArrowLeft'){
+           moveDodgerLeft()
+   }
+
+
+    if(e.key == 'ArrowRight'){
+
+         moveDodgerRight()
+    }
+   
+})
