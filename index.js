06b4bf64a70ed7d35145c255052e71874c91a5bd
kelly_maass <kmaass77627@gmail.com>
2023-07-05 18:24:06 -0500

    new

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-// Your code here
+
 let dodger = document.getElementById(`dodger`);
+dodger.style.backgroundColor = "#FF69B4"
+
+
+
+
+
+  
+
+
+
+
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
-    moveDodgerLeft()
+    moveDodgerLeft();
   } else if (e.key === "ArrowRight") {
-    moveDodgerRight()
+    moveDodgerRight();
   }
-});
-
-
+}); 
 
 function moveDodgerLeft() {
-let leftNumbers = dodger.style.left.replace("px", "");
-let left = parseInt(leftNumbers, 10);
-if (left > 0) {
-  dodger.style.left = `${left - 1}px`;
-}
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-}
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+} 
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.left.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers, 10);
   if (right < 300) {
     dodger.style.left = `${right + 1}px`;
   }
-
 }
